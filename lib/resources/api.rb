9ad819516dbbf3281dca73b39673235b176c51cd
Jenn Tustin <jenn.tustin@gmail.com>
2017-02-09 17:03:10 -0500

    Add post end points

---
+++ lib/resources/api.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'active_record'
+require 'yaml'
+require_relative '../models/foodcart'
+require_relative '../models/item'
+require_relative '../models/rating'
+
+database_config = ENV['DATABASE_URL']
+
+if database_config.blank?
+  database_config = YAML::load(File.open('config/database.yml'))
+end
+
+ActiveRecord::Base.establish_connection(database_config)
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+post '/api/foodcart' do
+  fc = Foodcart.new(name: params[:name], description: params[:description], image: params[:image] )
+  if fc.valid?
+    fc.save
+    [201, fc.to_json]
+  else
+    halt 400
+  end
+end
+
+post '/api/item' do
+  i = Item.new(foodcart_id: params[foodcart_id], name: params[:name], vegetarian: params[:vegetarian] )
+  if i.valid?
+    i.save
+    [201, i.to_json]
+
+  else
+    halt 400
+  end
+end
+
+post '/api/rating' do
+  r = Rating.new(foodcart_id: params[foodcart_id], score: params[:score], review: params[:review] )
+  if r.valid?
+    r.save
+    [201, r.to_json]
+
+  else
+    halt 400
+  end
+end
