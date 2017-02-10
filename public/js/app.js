e32955a1b61a2bb01d3ac97203aee1c9e3037965
Shane <shanemccoypdx@gmail.com>
2017-02-10 10:51:31 -0500

    FE // Updated app.js - connecting to test api. pulling data.

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@
 	////////////////////////////////////////////
 	// Work Flow ::::
 	// iifee ---> jq/ready ---> constr ---> init()
-	// init() --> bindEvents()
+	// init() --> bindEvents(getSearchInput)
 	// init() --> generateTemplate(input)
 	//
 	////////////////////////////////////////////
@@ -19,16 +19,24 @@
 	/*      CONSTRUCT    */
 	/*****************************************/
 	const FoodcartModule = function() {
+		const searchForm = document.querySelector('.search-form');
+		const apiKey = '230dad24aa940a540fb59d2c90298965'; // TEMP VARIABLE FOR TESTING **REMOVE
 
 		////////////////////////////////////////////
 		// CLASS :
 		////////////////////////////////////////////
 		class FoodcartDetails {
-			constructor(foodcartObj) {
 
+			// our keys  ||| object keys we are pulling from
+			constructor(foodcartObj) {
+				this.avgRating = foodcartObj.id;
+				this.image = `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${foodcartObj.poster_path}`;
+				this.name = foodcartObj.title;
+				this.description = foodcartObj.overview;
 				this.build();
-				// console.log(this);
+				console.log('class this --> ' + this);
 			}
+
 			////////////////////////////////////////////
 			// BUILD :: GENERATE TEMPLATE
 			build() {
@@ -38,10 +46,11 @@
 				const template = Handlebars.compile(source);
 
 				const context = {
-					// image: this.image,
-					// title: this.title,
-					// overview: this.overview,
-					// movieId: this.movieId
+					image: this.image,
+					name: this.name,
+					description: this.description,
+					avgRating: this.avgRating
+
 				};
 				const html = template(context);
 
@@ -49,13 +58,38 @@
 
 			} // end build func
 		} // end of CLASS
+		/////////////////////////////////////
+		// FUNCTION: API CALL // SEARCH RESULTS
+		/////////////////////////////////////
+		function APIRequest(query) {
+			query = encodeURIComponent(query);
+
+			$.get(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`)
+				.then((response) => {
+					console.log(response);
+					new FoodcartDetails(response.results[0]);
+				});
+		} // end function
+
 
 		/////////////////////////////////////
 		// FUNCTION: BIND EVENTS
 		/////////////////////////////////////
 		function bindEvents() {
+			searchForm.addEventListener('submit', () => {
+				event.preventDefault();
+				const searchValue = event.target[0].value; //grab value from input
+				console.log(searchValue);
+				APIRequest(searchValue); // pass value to APIRequest()
+				searchForm.reset(); // clear form
+			});
 
+			$('.template-container').on('click', '.btn-see-rate', function() {
+				// console.log('in');
+				$('.rr-container').removeClass('is-hidden'); // add fadeIn later
+			});
 		}
+
 
 		/////////////////////////////////////
 		// FUNCTION: INIT
